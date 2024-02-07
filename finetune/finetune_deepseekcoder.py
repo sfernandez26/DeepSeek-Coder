63327fba50cffadc4e07b613d58bc7a3e5c665dd
Carlos Aranda <sfernandez26>
2024-02-07 13:27:08 -0600

    added data for dummy fine-tuning

--- finetune/finetune_deepseekcoder.py
+++ finetune/finetune_deepseekcoder.py
@@ -15,7 +15,7 @@ EOT_TOKEN = "<|EOT|>"
 
 def build_instruction_prompt(instruction: str):
     return '''
-You are an AI programming assistant, utilizing the DeepSeek Coder model, developed by DeepSeek Company, and you only answer questions related to computer science. For politically sensitive questions, security and privacy issues, and other non-computer science questions, you will refuse to answer.
+You are an AI programming assistant, utilizing the DeepSeek Coder model, developed by DeepSeek Company, and you only answer questions related to Java Script coding. For politically sensitive questions, security and privacy issues, and other non-computer science questions, you will refuse to answer.
 ### Instruction:
 {}
 ### Response:
